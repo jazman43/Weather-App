3ee3cdc0d4b42d623d9b95abbe503e948ae9223f
jazman43 <supoww@hotmail.co.uk>
2023-09-30 15:30:23 +1300

    side-menu
    added template code to each section and created urls/ links for each of them

--- weather_app_cs203_a2/frontend/src/components/RainRadarWind.js
+++ weather_app_cs203_a2/frontend/src/components/RainRadarWind.js
@@ -6,7 +6,11 @@ import { BrowserRouter as Router, Switch, Route, Link, Outlet,
     Routes, } from "react-router-dom";
 
 import MainWeather from "./MainWeatherWindow";
-
+import WeatherWarings from "./WeaterWaringsWind";
+import LaundryForcast from "./LaundryForcastWind";
+import IdealFinishing from "./IdealFinishingWind";
+import FireDanger from "./FireDangerWind";
+import BBqForcast from "./BbqForcastWind";
 
 
 class RainRadar extends Component{
@@ -39,7 +43,22 @@ export default class SideBarMenu extends Component{
                   <Link to="/rain-radar">Rain Radar</Link>
                 </li>
                 <li>
-                  <Link to="/main-weather">weather</Link>
+                  <Link to="/main-weather">Weather</Link>
+                </li>
+                <li>
+                  <Link to="/weather-warings">Weather Warings</Link>
+                </li>
+                <li>
+                  <Link to="/laundry-forcast">Laundry Forcast</Link>
+                </li>
+                <li>
+                  <Link to="/ideal-finishing">Ideal Finishing</Link>
+                </li>
+                <li>
+                  <Link to="/fire-danger">Fire Danger</Link>
+                </li>
+                <li>
+                  <Link to="/bbq-forcast">BBq Forcast</Link>
                 </li>
 
                 {/* Add more links for additional pages as needed */}
@@ -51,6 +70,11 @@ export default class SideBarMenu extends Component{
               <Routes>
                 <Route exact path="/rain-radar" element={<RainRadar />} />
                 <Route exact path="/main-weather" element={<MainWeather />} />
+                <Route exact path="/weather-warings" element={<WeatherWarings />} />
+                <Route exact path="/laundry-forcast" element={<LaundryForcast />} />
+                <Route exact path="/ideal-finishing" element={<IdealFinishing />} />
+                <Route exact path="/fire-danger" element={<FireDanger />} />
+                <Route exact path="/bbq-forcast" element={<BBqForcast />} />
                 {/* Add more routes for additional pages as needed */}
               </Routes>
             </div>
--- weather_app_cs203_a2/frontend/urls.py
+++ weather_app_cs203_a2/frontend/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     
     path('', index),
     path('rain-radar', index),
-    path('main-weather', index)
+    path('main-weather', index),
+    path('weather-warings', index),
+    path('laundry-forcast', index),
+    path('ideal-finishing', index),
+    path('fire-danger', index),
+    path('bbq-forcast', index)
 ]
+
+
