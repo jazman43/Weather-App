97af33c0dfe1c41d51b0ae188b54f9806c09c69e
jazman43 <supoww@hotmail.co.uk>
2023-11-10 11:09:23 +1300

    beginning-to-add-api-requests
    attempt number one for request and response from weather api with django

--- weather_app_cs203_a2/api/views.py
+++ weather_app_cs203_a2/api/views.py
@@ -1,4 +1,9 @@
+import datetime
+
+import requests 
+
 from django.shortcuts import render
+
 from rest_framework import generics, status
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
@@ -7,6 +12,34 @@ from .models import RainRadarData
 from .serializers import RainRadarDataSerializer
 
 # Create your views here.
+def index(request):
+    API_KEY = '6cd8596a9e075cc1718aeee820c8d1fa'#open("API_KEY", "r").read()
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    forcast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid{}"
+
+    if request.method == "POST":
+        city1 = request.POST['city1']
+    else:
+        return render(request , "")
+
+
+
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+
+    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
+
+    weather_data = {
+        "city": city,
+        
+    }
+
+
+
+
 
 
 class RainRadarDataList(generics.ListCreateAPIView):
